e99a220ecfa1e392e4f4f87f8be4a4d8b7bbd0ca
Reva101 <115219445+Revaha0@users.noreply.github.com>
2024-09-10 00:31:58 +0300

    Add files via upload

---
+++ Sayılardan büyük olanı bulma.py
@@ -0,0 +1,11 @@
+# 2.ÖDEV 2.SORU 
+
+n = 13
+
+sayilar_listesi = [5, 12, 18, 24, 38, 49, 62, 97]
+
+n_ustunde_sayilar = [sayi for sayi in sayilar_listesi if sayi > n]
+
+
+print("n'den (", n, ") büyük olan sayılar:")
+print(n_ustunde_sayilar)
